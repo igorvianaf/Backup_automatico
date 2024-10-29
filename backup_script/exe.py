f3f8f79b5a66bcf8055e216cdd9b64cb889f8d33
igorvianaf <igor.vianaf1@hotmail.com>
2024-10-28 21:05:11 -0300

    Update exe.py

--- backup_script/exe.py
+++ backup_script/exe.py
@@ -7,11 +7,7 @@ destino = "D:\\backup"
 
 # Listar arquivos da pasta origem
 arquivos_origem = os.listdir(origem)
-arquivos_destino = os.listdir(destino)
 
-# Colocar arquivos em ordem para deletar apenas os dois mais antigos
-arquivos_origem.sort()
-arquivos_destino.sort()
 
 # Copiar arquivos da pasta origem para Backup
 for arquivo in arquivos_origem:
@@ -20,18 +16,3 @@ for arquivo in arquivos_origem:
     shutil.copy2(caminho_origem, caminho_destino)
 
 print(f'Arquivo salvo em {destino}')
-
-# Apagar apenas os dois primeiros arquivos da pasta destino, para que fiquem apenas dois na pasta backup
-if len(arquivos_destino) >= 4:
-    for arquivo in arquivos_destino[:-2]:
-
-        caminho_arquivo_antigo = os.path.join(destino, arquivo)
-        os.remove(caminho_arquivo_antigo)
-    print(f'Pasta {destino} atualizada com sucesso!')
-
-# Deletar os dois arquivos mais antigos da pasta origem
-if len(arquivos_origem) >= 4:
-    for arquivo in arquivos_origem[:-2]:
-        caminho_arquivo_antigo = os.path.join(origem, arquivo)
-        os.remove(caminho_arquivo_antigo)
-    print(f'Pasta {origem} atualizada com sucesso! ')
