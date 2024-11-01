c276fe18275449bcf3a34acea5a37afcf53aa136
igorvianaf <igor.vianaf1@hotmail.com>
2024-10-31 21:14:11 -0300

    Update script_backup.py

--- script_backup.py
+++ script_backup.py
@@ -2,8 +2,8 @@ import os
 import shutil
 
 # Inserir local de origem e destino do arquivo de backup
-origem = "C:\\Users\\igorv\\OneDrive\\Área de Trabalho\\arquivos"
-destino = "D:\\backup"
+origem = "C:\\Users\\igorv\\OneDrive\\Área de Trabalho\\origem"
+destino = "C:\Users\igorv\OneDrive\Área de Trabalho\destino"
 
 # Listar arquivos da pasta origem
 arquivos_origem = os.listdir(origem)
@@ -14,6 +14,6 @@ for arquivo in arquivos_origem:
     caminho_origem = os.path.join(origem, arquivo)
     caminho_destino = os.path.join(destino, arquivo)
     shutil.copy2(caminho_origem, caminho_destino)
-    # Usamos o copytree quando queremos copiar um diretorio inteiro
+# Usamos o copytree quando queremos copiar um diretorio inteiro
 
 print(f'Arquivo salvo em {destino}')
