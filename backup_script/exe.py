87e208fc4526bb7ae23355db3be64688136f97f7
igorvianaf <igor.vianaf1@hotmail.com>
2024-10-31 22:15:15 -0300

    Update exe.py

--- backup_script/exe.py
+++ backup_script/exe.py
@@ -2,16 +2,12 @@ import os
 import shutil
 
 # Inserir local de origem e destino do arquivo de backup
-origem = "C:\\Users\\igorv\\OneDrive\\Área de Trabalho\\arquivos"
-destino = "D:\\backup"
+origem = "C:\\Users\\igorv\\OneDrive\\Área de Trabalho\\origem"
+destino = "C:\\Users\\igorv\\OneDrive\\Área de Trabalho\\destino"
 
 # Listar arquivos da pasta origem
 arquivos_origem = os.listdir(origem)
 arquivos_destino = os.listdir(destino)
-
-# Colocar arquivos em ordem para deletar apenas os dois mais antigos
-arquivos_origem.sort()
-arquivos_destino.sort()
 
 # Copiar arquivos da pasta origem para Backup
 for arquivo in arquivos_origem:
@@ -24,7 +20,6 @@ print(f'Arquivo salvo em {destino}')
 # Apagar apenas os dois primeiros arquivos da pasta destino, para que fiquem apenas dois na pasta backup
 if len(arquivos_destino) >= 4:
     for arquivo in arquivos_destino[:-2]:
-
         caminho_arquivo_antigo = os.path.join(destino, arquivo)
         os.remove(caminho_arquivo_antigo)
     print(f'Pasta {destino} atualizada com sucesso!')
